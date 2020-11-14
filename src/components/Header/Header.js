8979f18272f867375e34b49cd3d68fd399ee387f
Sheila Leon <sheilaleon.au@gmail.com>
2020-11-14 17:44:25 +1100

    Applying original filename structure

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import useSiteMetadata from '../../hooks/useSiteMetadata';
-import ToggleColourScheme from '../lay-out/ToggleColourScheme';
+import ToggleColourScheme from '../Layout/ToggleColourScheme';
 import Brand from '../Brand/Brand';
 
 const Header = () => {
