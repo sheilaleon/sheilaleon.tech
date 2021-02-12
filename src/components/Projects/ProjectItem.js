13ce263d031a8703393da4f417fbb4ad9e81ca5a
Sheila Leon <sheilaleon.au@gmail.com>
2021-02-12 19:48:48 +1100

    Add FSO project, update Now

--- src/components/Projects/ProjectItem.js
+++ src/components/Projects/ProjectItem.js
@@ -10,11 +10,15 @@ const ProjectItem = ({
   githubLink,
 }) => (
   <div className="project">
-    <h4>
-      <a href={demoLink} rel="noopener noreferrer" target="_blank">
-        {projectName}
-      </a>
-    </h4>
+    {demoLink != null ? (
+      <h4>
+        <a href={demoLink} rel="noopener noreferrer" target="_blank">
+          {projectName}
+        </a>
+      </h4>
+    ) : (
+      <h4>{projectName}</h4>
+    )}
     <p className="text-sm">{description}</p>
     <ul className="tags">
       {tags.map((tag, index) => (
@@ -22,24 +26,28 @@ const ProjectItem = ({
       ))}
     </ul>
     <div className="preview">
-      <a
-        href={githubLink}
-        rel="noopener noreferrer"
-        target="_blank"
-        className="preview preview-link"
-      >
-        <Icon name="code" />
-        <span>View on GitHub</span>
-      </a>
-      <a
-        href={demoLink}
-        rel="noopener noreferrer"
-        target="_blank"
-        className="preview preview-link"
-      >
-        <Icon name="external-link" />
-        <span>View Project</span>
-      </a>
+      {githubLink != null ? (
+        <a
+          href={githubLink}
+          rel="noopener noreferrer"
+          target="_blank"
+          className="preview preview-link"
+        >
+          <Icon name="code" />
+          <span>View on GitHub</span>
+        </a>
+      ) : null}
+      {demoLink != null ? (
+        <a
+          href={demoLink}
+          rel="noopener noreferrer"
+          target="_blank"
+          className="preview preview-link"
+        >
+          <Icon name="external-link" />
+          <span>View Project</span>
+        </a>
+      ) : null}
     </div>
   </div>
 );
